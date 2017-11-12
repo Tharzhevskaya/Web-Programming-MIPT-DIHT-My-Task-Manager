f06beb931c04eb39ee924f908c8905c7d3d53d6c
olga <belova.olga98@mail.ru>
2017-11-12 18:12:25 +0300

    working on task-3 for web-programming

---
+++ app.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function() 
+{
+	var i = 0;
+	$("#root").append("<ul></ul>")
+	$("ul").append("<li></li>")
+	$("li").append("<span>Сделать задание #3 по web-программированию</span>")
+	$("li").append("<button>Удалить</button>").click(function() 
+	{
+		$(this).remove();
+	});
+	$("#root").append("<input id='add_task_input'>")
+	$("#root").append("<button id='add_task'>Ввести</button>")
+	$("#add_task").click(function() 
+	{		
+		i = i + 1;
+		var inputstring = $("input").val();
+		$("ul").append($("<li><span></span></li>").attr("id", i));
+		$("#" + i).text(inputstring);
+		$("#" + i).append("<button>Удалить</button>").click(function() 
+		{
+			$(this).remove();
+		});
+
+	});
+});
